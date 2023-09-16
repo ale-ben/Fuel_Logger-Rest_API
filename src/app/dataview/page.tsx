4fab8db1cd8378980ca6af9029936203325511a4
Alessandro Benetton <aleben98@gmail.com>
2023-09-16 10:25:16 +0200

    Move data to dataview page

--- src/app/dataview/page.tsx
+++ src/app/dataview/page.tsx
@@ -1,8 +1,14 @@
-import { redirect } from 'next/navigation';
+import { FuelLogOverview } from '@/models/FuelLog';
+import { getFuelLogs } from '@/serverActions/fuelDB';
+import LogElement from '@/app/components/logElement';
 
-const DataView = () => {
-  redirect('/wip?from=Data View');
-  return <div>DataView</div>;
-};
-
-export default DataView;
+export default async function DataView() {
+  const logs: FuelLogOverview[] = await getFuelLogs();
+  return (
+    <div className="flex grow flex-row flex-wrap justify-items-stretch gap-4 p-5">
+      {logs.map((log: FuelLogOverview) => (
+        <LogElement element={log} key={log.id} />
+      ))}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-import { FuelLog, FuelLogOverview } from '@/models/FuelLog';
-import { getFuelLogs } from '@/serverActions/fuelDB';
-import LogElement from './components/logElement';
 
-export default async function Home() {
-  const logs: FuelLogOverview[] = await getFuelLogs();
+export default async function Dashboard() {
   return (
-    <div className="flex grow flex-row flex-wrap justify-items-stretch gap-4 p-5">
-      {logs.map((log: FuelLogOverview) => (
-        <LogElement element={log} key={log.id} />
-      ))}
+    <div className="text-2xl">
+      One day I will be a nice dashboard with all relevant info
     </div>
   );
 }

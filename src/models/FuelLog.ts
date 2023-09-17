0ac504abe2bd34d82989ae44ba4f018158f79426
Alessandro Benetton <aleben98@gmail.com>
2023-09-17 18:29:08 +0200

    Implemented DB read

--- src/models/FuelLog.ts
+++ src/models/FuelLog.ts
@@ -11,11 +11,11 @@ export interface FuelLog {
 }
 
 export interface FuelLogOverview {
-	key: string;
-	mileage: number;
-	date: Date;
-	liters: number;
-	price: number;
+  key: string;
+  mileage: number;
+  date: Date;
+  liters: number;
+  price: number;
 }
 
 export interface ErrorLog {
@@ -29,20 +29,66 @@ export interface ErrorLog {
   priceRate?: number;
 }
 
+export function isFuelLogEntry(obj: any): obj is FuelEntry {
+  const checkObj = obj !== undefined;
+  const checkDate = obj.date instanceof Date;
+  const checkLiters = Number.isFinite(obj.liters);
+  const checkPrice = Number.isFinite(obj.price);
+  if (checkObj && checkDate && checkLiters && checkPrice) {
+    return true;
+  } else {
+    console.log(`Invalid fuel log entry ${JSON.stringify(obj)}
+	obj: ${checkObj}
+	obj.date instanceof Date: ${checkDate}
+	Number.isFinite(obj.liters): ${checkLiters}
+	Number.isFinite(obj.price): ${checkPrice}
+	  `);
+    return false;
+  }
+}
+
+export function isFuelLog(obj: any): obj is FuelLog {
+  const checkObj = obj !== undefined;
+  const checkMileage = Number.isFinite(obj.mileage);
+  const checkEntries = obj.entries !== undefined;
+  const checkEntriesArray = obj.entries instanceof Array;
+  const checkEntriesEvery =
+    obj.entries &&
+    obj.entries instanceof Array &&
+    obj.entries.every(isFuelLogEntry);
+  if (
+    checkObj &&
+    checkMileage &&
+    checkEntries &&
+    checkEntriesArray &&
+    checkEntriesEvery
+  ) {
+    return true;
+  } else {
+    console.log(`Invalid fuel log ${JSON.stringify(obj)}
+	obj: ${checkObj}
+	Number.isFinite(obj.mileage): ${checkMileage}
+	obj.entries: ${checkEntries}
+	obj.entries instanceof Array: ${checkEntriesArray}
+	obj.entries.every(isFuelLogEntry): ${checkEntriesEvery}`);
+    return false;
+  }
+}
+
 /**
  * Converts a fuel log to a fuel log overview, aggregating all the fuel entries
  * @param log The fuel log
  * @returns A FuelLogOverview
  */
 export function getFuelLogOverview(log: FuelLog): FuelLogOverview | undefined {
-	if (!log.key) return undefined;
-	return {
-		key: log.key,
-		mileage: log.mileage,
-		date: log.entries[0].date,
-		liters: log.entries.map(entry => entry.liters).reduce((a, b) => a + b, 0),
-		price: log.entries.map(entry => entry.price).reduce((a, b) => a + b, 0),
-	};
+  if (!log.key) return undefined;
+  return {
+    key: log.key,
+    mileage: log.mileage,
+    date: log.entries[0].date,
+    liters: log.entries.map((entry) => entry.liters).reduce((a, b) => a + b, 0),
+    price: log.entries.map((entry) => entry.price).reduce((a, b) => a + b, 0),
+  };
 }
 
 /**
--- src/serverActions/fuelDB.ts
+++ src/serverActions/fuelDB.ts
@@ -1,6 +1,11 @@
 'use server';
 
-import { FuelLog, FuelLogOverview, getFuelLogOverview } from '@/models/FuelLog';
+import {
+  FuelLog,
+  FuelLogOverview,
+  getFuelLogOverview,
+  isFuelLog,
+} from '@/models/FuelLog';
 
 import { Deta } from 'deta'; // import Deta
 
@@ -71,15 +76,6 @@ const templateLogs: FuelLog[] = [
   },
 ];
 
-export async function getFuelLogs(
-  limit?: number,
-  offset?: number,
-): Promise<FuelLogOverview[]> {
-  return templateLogs
-    .map((log) => getFuelLogOverview(log))
-    .filter((log) => log !== undefined) as FuelLogOverview[];
-}
-
 function fuelLogToStorable(log: FuelLog): StorableFuelLog {
   return {
     ...log,
@@ -90,6 +86,80 @@ function fuelLogToStorable(log: FuelLog): StorableFuelLog {
       };
     }),
   };
+}
+
+function storableToFuelLog(storableLog: StorableFuelLog): FuelLog {
+  return {
+    ...storableLog,
+    entries: storableLog.entries.map((elem) => {
+      return {
+        ...elem,
+        date: new Date(elem.date),
+      };
+    }),
+  };
+}
+
+function isStorableFuelLogEntry(obj: any): obj is StorableFuelEntry {
+  const checkObj = obj !== undefined;
+  const checkDate = Number.isFinite(obj.date);
+  const checkLiters = Number.isFinite(obj.liters);
+  const checkPrice = Number.isFinite(obj.price);
+  if (checkObj && checkDate && checkLiters && checkPrice) {
+    return true;
+  } else {
+    console.log(`Invalid storable fuel log entry ${JSON.stringify(obj)}
+	  obj: ${checkObj}
+	  Number.isFinite(obj.date): ${checkDate}
+	  Number.isFinite(obj.liters): ${checkLiters}
+	  Number.isFinite(obj.price): ${checkPrice}
+		`);
+    return false;
+  }
+}
+
+function isStorableFuelLog(obj: any): obj is StorableFuelLog {
+  const checkObj = obj !== undefined;
+  const checkMileage = Number.isFinite(obj.mileage);
+  const checkEntries = obj.entries !== undefined;
+  const checkEntriesArray = obj.entries instanceof Array;
+  const checkEntriesEvery =
+    obj.entries &&
+    obj.entries instanceof Array &&
+    obj.entries.every(isStorableFuelLogEntry);
+  if (
+    checkObj &&
+    checkMileage &&
+    checkEntries &&
+    checkEntriesArray &&
+    checkEntriesEvery
+  ) {
+    return true;
+  } else {
+    console.log(`Invalid storable fuel log ${JSON.stringify(obj)}
+	  obj: ${checkObj}
+	  Number.isFinite(obj.mileage): ${checkMileage}
+	  obj.entries: ${checkEntries}
+	  obj.entries instanceof Array: ${checkEntriesArray}
+	  obj.entries.every(isFuelLogEntry): ${checkEntriesEvery}`);
+    return false;
+  }
+}
+
+export async function getFuelLogs(
+  limit?: number,
+  offset?: number,
+): Promise<FuelLogOverview[] | undefined> {
+  const logs = await fuelDB.fetch();
+  if (logs.count === 0) return undefined;
+  const convertedLogs = logs.items.map((elem) => {
+    if (isStorableFuelLog(elem)) return storableToFuelLog(elem);
+    else {
+      console.log('Invalid fuel log ' + JSON.stringify(elem));
+      undefined;
+    }
+  });
+  return convertedLogs.filter((elem) => elem !== undefined).map((elem) => getFuelLogOverview(elem as FuelLog)) as FuelLogOverview[];
 }
 
 export async function saveFuelLogs(logs: FuelLog[]) {

25b918ee218115ecf7392b087a564324893719eb
Alessandro Benetton <aleben98@gmail.com>
2023-11-24 11:16:19 +0100

    [feature/FL-13] Migrate from flowbite to nextui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,28 @@
 import Navbar from './components/navbar';
 import './globals.css';
 import type { Metadata } from 'next';
+import { Providers } from './providers';
 
 export const metadata: Metadata = {
-  title: 'Fuel Logger',
-  description: 'Tool for tracking fuel consumption of a vehicle',
+	title: 'Fuel Logger',
+	description: 'Tool for tracking fuel consumption of a vehicle'
 };
 
 export default function RootLayout({
-  children,
+	children
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className="flex h-screen w-screen flex-col">
-        <Navbar />
-        <div className="h-full w-full overflow-hidden border-2 border-blue-300">
-          {children}
-        </div>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className="dark">
+			<Providers>
+				<body className="flex h-screen w-screen flex-col">
+					<Navbar />
+					<div className="h-full w-full overflow-hidden border-2 border-blue-300">
+						{children}
+					</div>
+				</body>
+			</Providers>
+		</html>
+	);
 }

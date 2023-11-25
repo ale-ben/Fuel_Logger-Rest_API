3a634ccc13e610ab4c98d8c289a3bafe7bf54883
Alessandro Benetton <aleben98@gmail.com>
2023-11-25 18:36:14 +0100

    [feature/FL-29] Working storage and retrieval

--- src/models/FuelLog.ts
+++ src/models/FuelLog.ts
@@ -1,23 +1,23 @@
 export interface FuelEntry {
-	id: number;
+	key: number;
 	date: number;
 	amount: number;
 	price: number;
 }
 
 export interface FuelLog {
-	id: number;
+	key: number;
 	mileage: number;
 	entries: FuelEntry[];
 }
 
 export const DummyLogs: FuelLog[] = [
 	{
-		id: 0,
+		key: 0,
 		mileage: 561.1,
 		entries: [
 			{
-				id: 0,
+				key: 0,
 				date: new Date('2023-06-24').getTime(),
 				amount: 33.19,
 				price: 60.37
@@ -25,17 +25,17 @@ export const DummyLogs: FuelLog[] = [
 		]
 	},
 	{
-		id: 1,
+		key: 1,
 		mileage: 572.7,
 		entries: [
 			{
-				id: 0,
+				key: 0,
 				date: new Date('2023-07-21').getTime(),
 				amount: 32.84,
 				price: 60.72
 			},
 			{
-				id: 1,
+				key: 1,
 				date: new Date('2023-07-23').getTime(),
 				amount: 12.84,
 				price: 22.72
@@ -43,11 +43,11 @@ export const DummyLogs: FuelLog[] = [
 		]
 	},
 	{
-		id: 2,
+		key: 2,
 		mileage: 523.1,
 		entries: [
 			{
-				id: 0,
+				key: 0,
 				date: new Date('2023-08-27').getTime(),
 				amount: 33.42,
 				price: 63.46
@@ -55,11 +55,11 @@ export const DummyLogs: FuelLog[] = [
 		]
 	},
 	{
-		id: 3,
+		key: 3,
 		mileage: -1,
 		entries: [
 			{
-				id: 0,
+				key: 0,
 				date: new Date('2023-09-08').getTime(),
 				amount: 12.05,
 				price: 23.24
@@ -78,4 +78,65 @@ export function GetAmount(log: FuelLog): number {
 
 export function GetPrice(log: FuelLog): number {
 	return log.entries.reduce((sum, entry) => sum + entry.price, 0);
+}
+
+// Type Guards
+function IsFuelEntry(obj: unknown): obj is FuelEntry {
+	const tmpObj = obj as FuelEntry;
+
+	if (tmpObj.key === undefined || typeof tmpObj.key !== 'number') {
+		return false;
+	}
+
+	if (tmpObj.date === undefined || typeof tmpObj.date !== 'number') {
+		return false;
+	}
+
+	if (tmpObj.amount === undefined || typeof tmpObj.amount !== 'number') {
+		return false;
+	}
+
+	if (tmpObj.price === undefined || typeof tmpObj.price !== 'number') {
+		return false;
+	}
+
+	return true;
+}
+
+export function IsFuelLog(obj: unknown): obj is FuelLog {
+	const tmpObj = obj as FuelLog;
+
+	if (tmpObj.key === undefined || typeof tmpObj.key !== 'number') {
+		return false;
+	}
+
+	if (tmpObj.mileage === undefined || typeof tmpObj.mileage !== 'number') {
+		return false;
+	}
+
+	if (tmpObj.entries === undefined || !Array.isArray(tmpObj.entries)) {
+		return false;
+	}
+
+	tmpObj.entries.forEach((entry) => {
+		if (!IsFuelEntry(entry)) {
+			return false;
+		}
+	});
+
+	return true;
+}
+
+export function IsFuelLogArray(obj: unknown): obj is FuelLog[] {
+	if (!Array.isArray(obj)) {
+		return false;
+	}
+
+	obj.forEach((log) => {
+		if (!IsFuelLog(log)) {
+			return false;
+		}
+	});
+
+	return true;
 }
---
+++ src/serverActions/FuelLogStorage.ts
@@ -0,0 +1,27 @@
+'use server';
+
+import { FuelLog, IsFuelLog, IsFuelLogArray } from '@/models/FuelLog';
+import { Deta } from 'deta';
+import { ObjectType } from 'deta/dist/types/types/basic';
+
+const deta = Deta();
+const fuelDB = deta.Base('fuel');
+
+export async function getFuelLogs(): Promise<FuelLog[] | undefined> {
+	const res = await fuelDB.fetch({});
+
+	console.log(res);
+	if (res !== undefined && IsFuelLogArray(res.items)) return res.items as FuelLog[];
+	else return undefined;
+}
+
+export async function getFuelLog(id: number): Promise<FuelLog | undefined> {
+	const res = await fuelDB.get(id.toString());
+
+	if (res !== undefined && IsFuelLog(res)) return res as FuelLog;
+	else return undefined;
+}
+
+export async function createFuelLog(log: FuelLog): Promise<void> {
+	await fuelDB.put(log as unknown as ObjectType);
+}

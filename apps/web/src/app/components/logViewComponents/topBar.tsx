f682a663b276e913ee33d491a1cad79f7b9bd37d
Alessandro Benetton <aleben98@gmail.com>
2024-01-05 17:19:58 +0100

    [feature/FL-1] basic date picker ui

--- apps/web/src/app/components/logViewComponents/topBar.tsx
+++ apps/web/src/app/components/logViewComponents/topBar.tsx
@@ -5,17 +5,81 @@ import { FuelLogModalContext } from '@/context/FuelLogModalContext';
 import { RevalidatePath } from '@/serverActions/genericActions';
 import { defaultFuelLog } from '@fuel-logger/dbutils';
 import { usePathname } from 'next/navigation';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { LuPlus, LuRefreshCw } from 'react-icons/lu';
 import EditLogModal from './editLogModal';
+
+import { Calendar } from '@/components/ui/calendar';
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger
+} from '@/components/ui/popover';
+import { cn } from '@/lib/utils';
+import { CalendarIcon } from '@radix-ui/react-icons';
+import { format } from 'date-fns';
+import { DateRange } from 'react-day-picker';
 
 const TopBar = () => {
 	const { dispatch } = useContext(FuelLogModalContext);
 	const path = usePathname();
 
+	const [date, setDate] = useState<DateRange | undefined>({
+		from: undefined,
+		to: new Date()
+	});
+
 	return (
-		<div className="flex flex-row justify-between">
-			<div>Filter by date</div>
+		<div className="flex flex-row justify-between align-middle">
+			<div className="flex flex-row">
+				<Popover>
+					<PopoverTrigger asChild>
+						<Button
+							id="date"
+							variant={'outline'}
+							className={cn(
+								'justify-start text-left font-normal',
+								!date && 'text-muted-foreground'
+							)}
+						>
+							<CalendarIcon className="mr-2 h-4 w-4" />
+							{date?.from ? (
+								date.to ? (
+									<>
+										{format(date.from, 'LLL dd, y')} -{' '}
+										{format(date.to, 'LLL dd, y')}
+									</>
+								) : (
+									format(date.from, 'LLL dd, y')
+								)
+							) : (
+								<span>Pick a date</span>
+							)}
+						</Button>
+					</PopoverTrigger>
+					<PopoverContent className="w-auto p-0" align="start">
+						<Calendar
+							initialFocus
+							mode="range"
+							defaultMonth={date?.from}
+							selected={date}
+							onSelect={setDate}
+							numberOfMonths={2}
+						/>
+					</PopoverContent>
+				</Popover>
+				<Button
+					variant="outline"
+					className="ml-2"
+					disabled={
+						date === undefined ||
+						date.from === undefined ||
+						date.to === undefined
+					}
+				>
+					Submit
+				</Button>
+			</div>
 			<div>
 				<Button
 					variant="outline"
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,6 +2,7 @@ import { ScrollArea } from '@/components/ui/scroll-area';
 import { getFuelLogs } from '@fuel-logger/dbutils';
 import SingleLog from './components/logViewComponents/singleLog';
 import TopBar from './components/logViewComponents/topBar';
+import { Separator } from '@/components/ui/separator';
 
 export default async function LogView() {
 	const logs = await getFuelLogs();
@@ -11,6 +12,7 @@ export default async function LogView() {
 			<div className="text-center text-2xl">Fuel Logs</div>
 			<div className="m-5 grow">
 				<TopBar />
+				<Separator className='my-5'/>
 				<ScrollArea className="mt-5 h-[550px] w-full">
 					<div className="flex flex-row flex-wrap justify-evenly gap-2 px-3">
 						{logs ? (

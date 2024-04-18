f8c47405a2f2611ef3d885732237d0271776d532
Alessandro Benetton <aleben98@gmail.com>
2024-04-18 10:01:02 +0200

    Frankenstein

--- TelegramBot/src/main.rs
+++ TelegramBot/src/main.rs
@@ -1,15 +1,61 @@
-use teloxide::prelude::*;
+use frankenstein::GetUpdatesParams;
+use frankenstein::ReplyParameters;
+use frankenstein::SendMessageParams;
+use frankenstein::TelegramApi;
+use frankenstein::{Api, UpdateContent};
 
-#[tokio::main]
-async fn main() {
-    pretty_env_logger::init();
-    log::info!("Starting throw dice bot...");
+extern crate dotenv;
 
-    let bot = Bot::from_env();
+use dotenv::dotenv;
+use std::env;
 
-    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        bot.send_dice(msg.chat.id).await?;
-        Ok(())
-    })
-    .await;
+fn main() {
+
+	dotenv().ok();
+
+	let TOKEN = env::var("FL_TELEGRAM_TOKEN").unwrap();
+
+    let api = Api::new(&TOKEN);
+
+    let update_params_builder = GetUpdatesParams::builder();
+    let mut update_params = update_params_builder.clone().build();
+
+    loop {
+        let result = api.get_updates(&update_params);
+
+        println!("result: {result:?}");
+
+        match result {
+            Ok(response) => {
+				
+
+                for update in response.result {
+                    if let UpdateContent::Message(message) = update.content {
+						send_message(&api, message.chat.id, message.message_id, "Hello")
+					}
+                    update_params = update_params_builder
+                        .clone()
+                        .offset(update.update_id + 1)
+                        .build();
+                }
+            }
+            Err(error) => {
+                println!("Failed to get updates: {error:?}");
+            }
+        }
+    }
+}
+
+fn send_message(api: &Api, chat: i64, message: i32, text: &str) {
+    let reply_parameters = ReplyParameters::builder().message_id(message).build();
+
+    let send_message_params = SendMessageParams::builder()
+        .chat_id(chat)
+        .text("hello")
+        .reply_parameters(reply_parameters)
+        .build();
+
+    if let Err(err) = api.send_message(&send_message_params) {
+        println!("Failed to send message: {err:?}");
+    }
 }

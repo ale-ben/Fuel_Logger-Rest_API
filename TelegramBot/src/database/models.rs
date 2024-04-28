0106d2f26110604377045559b5fce00a5ded5bbf
Alessandro Benetton <aleben98@gmail.com>
2024-04-28 22:52:48 +0200

    Parsing between types

--- TelegramBot/src/database/models.rs
+++ TelegramBot/src/database/models.rs
@@ -3,19 +3,19 @@ use diesel::{data_types::PgTimestamp, prelude::*};
 #[derive(Queryable, Selectable)]
 #[diesel(table_name = crate::database::schema::fuellogs)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
-pub struct FuelLog {
+pub struct PGFuelLog {
     pub id: i32,
-	pub odometer: f32,
-	pub flagged: bool,
+    pub odometer: f32,
+    pub flagged: bool,
 }
 
 #[derive(Queryable, Selectable)]
 #[diesel(table_name = crate::database::schema::fuelentries)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
-pub struct FuelEntry {
+pub struct PGFuelEntry {
     pub id: i32,
-	pub date: PgTimestamp,
-	pub amount: f32,
-	pub cost: f32,
-	pub fuellog: i32,
+    pub date: PgTimestamp,
+    pub amount: f32,
+    pub cost: f32,
+    pub fuellog: i32,
 }
--- TelegramBot/src/utils/fuel_log.rs
+++ TelegramBot/src/utils/fuel_log.rs
@@ -1,33 +1,75 @@
+use diesel::data_types::PgTimestamp;
 use time::OffsetDateTime;
 
-pub struct FuelEntry{
-	pub date: OffsetDateTime,
-	pub amount: f32,
-	pub cost: f32,
+use crate::database::models::{PGFuelEntry, PGFuelLog};
+
+pub struct FuelEntry {
+    pub date: OffsetDateTime,
+    pub amount: f32,
+    pub cost: f32,
 }
 
 pub struct FuelLog {
-	pub odometer: f32,
-	is_flagged: bool,
-	pub entries: Vec<FuelEntry>
+    pub odometer: f32,
+    is_flagged: bool,
+    pub entries: Vec<FuelEntry>,
 }
 
 impl FuelEntry {
-	pub fn new() -> Self {
-		Self {
-			date: OffsetDateTime::now_utc(),
-			amount: 0_f32,
-			cost: 0_f32,
-		}
-	}
+    pub fn new() -> Self {
+        Self {
+            date: OffsetDateTime::now_utc(),
+            amount: 0_f32,
+            cost: 0_f32,
+        }
+    }
+
+    pub fn as_pg_fuel_entry(self, log_id: i32) -> PGFuelEntry {
+        PGFuelEntry {
+            id: -1,
+            date: PgTimestamp(self.date.unix_timestamp()),
+            amount: self.amount,
+            cost: self.cost,
+            fuellog: log_id,
+        }
+    }
+
+    pub fn from_pg_fuel_entry(fuel_entry: PGFuelEntry) -> Result<Self, ()> {
+        if let Ok(date) = OffsetDateTime::from_unix_timestamp(fuel_entry.date.0) {
+            Ok(Self {
+                date,
+                amount: fuel_entry.amount,
+                cost: fuel_entry.cost,
+            })
+        } else {
+            error!("Unable to parse timestamp {}", fuel_entry.date.0);
+            Err(())
+        }
+    }
 }
 
 impl FuelLog {
-	pub fn new() -> Self {
-		Self {
-			odometer: 0_f32,
-			is_flagged: true,
-			entries: vec![FuelEntry::new()]
-		}
-	}
+    pub fn new() -> Self {
+        Self {
+            odometer: 0_f32,
+            is_flagged: true,
+            entries: vec![FuelEntry::new()],
+        }
+    }
+
+    pub fn as_pg_fuel_log(self) -> PGFuelLog {
+        PGFuelLog {
+            id: -1,
+            odometer: self.odometer,
+            flagged: self.is_flagged,
+        }
+    }
+
+    pub fn from_pg_fuel_log(fuel_log: PGFuelLog) -> Self {
+        Self {
+            odometer: fuel_log.odometer,
+            is_flagged: fuel_log.flagged,
+            entries: vec![],
+        }
+    }
 }

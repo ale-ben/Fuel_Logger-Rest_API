66ec1d7220600c488bbafac061c3966933d6e22b
Alessandro Benetton <aleben98@gmail.com>
2024-05-25 19:32:10 +0200

    Init swagger autogen

--- restApi/src/index.ts
+++ restApi/src/index.ts
@@ -1,11 +1,15 @@
 // src/index.js
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../docs/swagger-output.json';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Express + TypeScript Server");
---
+++ restApi/src/swagger.mjs
@@ -0,0 +1,18 @@
+import swaggerAutogen from 'swagger-autogen';
+import packageJson from '../package.json' with { type: "json" };;
+
+const doc = {
+    info: {
+        title: 'Fuel Logger',
+        version: packageJson.version
+    },
+    host: 'localhost:3000'
+};
+
+const outputFile = '../docs/swagger-output.json';
+const routes = ['./index.ts'];
+
+/* NOTE: If you are using the express Router, you must pass in the 'routes' only the
+root file where the route starts, such as index.js, app.js, routes.js, etc ... */
+
+swaggerAutogen()(outputFile, routes, doc);
